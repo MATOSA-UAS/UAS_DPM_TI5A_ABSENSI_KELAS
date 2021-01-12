55cca11070b67da53e870c377afcc86475b16e56
ATOMXXX112 <atomxxx647@gmail.com>
2021-01-12 22:12:20 +0700

    upload project

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/absensikelas/Izin.java
@@ -0,0 +1,26 @@
+package ac.id.atmaluhur.mhs.absensikelas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Izin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.halaman_izin);
+
+        Button balik = (Button)findViewById(R.id.ButtonBalik) ;
+        balik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Izin.this, Pilih.class);
+                startActivity(i);
+            }
+        });
+    }
+}
